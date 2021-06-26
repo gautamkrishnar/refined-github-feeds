6600bdc0f170af4a4a32e0465a4a4c4eeb8dd20e
gautamkrishnar <r.gautamkrishna@gmail.com>
2021-06-27 00:59:41 +0530

    removed unwanted async, added docs

--- app/scripts/pagestyle.js
+++ app/scripts/pagestyle.js
@@ -1,10 +1,22 @@
+/*
+ This file will be executed on Github.com when you visit the site
+ */
+
 import {InitAndReturnStates, CSSStyles, STYLE_ID} from "./shared";
 
+/**
+ * This function will be executed on Github.com, it will create a
+ * <style></style> tag with all the styles generated with the feature
+ * flags that user configured via the extension and appends the generated
+ * style tag to the document.head
+ * @return {Promise<void>}
+ */
 async function setRGHFSavedStyles() {
     const savedState = await InitAndReturnStates();
     const extStyle = document.createElement('style');
-    extStyle.id = STYLE_ID;
-    Object.keys(savedState).map(async (key)=> {
+    extStyle.id = STYLE_ID; // Assigning Id for easy removal
+    Object.keys(savedState).forEach((key)=> {
+        // Generating style sheet based on saved extension settings
         const status = savedState[key];
         if (!status) {
             extStyle.textContent =  extStyle.innerText + CSSStyles[key] + '\n\n';
--- app/scripts/shared.js
+++ app/scripts/shared.js
@@ -1,3 +1,9 @@
+/*
+ Shared file with utilities and constants
+ */
+
+// CSS Styles required to hide various feed items
+// Key names should match the checkbox id in the popup.html
 export const CSSStyles = {
     releases: `#dashboard .release { display: none; }`,
     stars: `#dashboard .watch_started { display: none; }`,
@@ -6,10 +12,26 @@ export const CSSStyles = {
 };
 
 export const STORAGE_KEY = 'refinedGithubData';
-export const STYLE_ID = 'rRGHFSavedStyles';
+export const STYLE_ID = 'RGHFSavedStyles';
 
+
+/**
+ * Refined GitHub feeds Extension Storage object
+ * @typedef {Object} ExtensionSavedData
+ * @property {boolean} forks - Forks feed status
+ * @property {boolean} stars - Stars feed status
+ * @property {boolean} follows - Follows feed status
+ * @property {boolean} releases - Releases feed status
+ */
+
+/**
+ * Initializes the extension's storage and sets the saved
+ * data states if this is ran for the first time.
+ * Will return the saved states if the storage is already initialized.
+ * @return {Promise<ExtensionSavedData>}
+ */
 export async function InitAndReturnStates() {
-    let SavedState = null;
+    let SavedState;
     const savedData = await browser.storage.local.get(STORAGE_KEY);
     if (savedData && savedData[STORAGE_KEY]) {
         // Read saved state from local storage
