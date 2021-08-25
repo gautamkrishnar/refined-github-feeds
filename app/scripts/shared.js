c0c295af1998880ff8fcec368b794d8f02eb727d
gautamkrishnar <r.gautamkrishna@gmail.com>
2021-08-25 18:44:10 +0530

    added hiding of sponsors

--- app/scripts/shared.js
+++ app/scripts/shared.js
@@ -1,15 +1,24 @@
 /*
  Shared file with utilities and constants
  */
+/**
+ * Builds and sends css required to hide the element
+ * @param id {string}
+ * @return {string}
+ */
+const getCSSStyle = (id) => {
+    return `#dashboard .${id} { display: none; }`;
+}
 
 // CSS Styles required to hide various feed items
 // Key names should match the checkbox id in the popup.html
 export const CSSStyles = {
-    releases: `#dashboard .release { display: none; }`,
-    stars: `#dashboard .watch_started { display: none; }`,
-    follows: `#dashboard .follow { display: none; }`,
-    forks: `#dashboard .fork { display: none; }`,
-    push: `#dashboard .push { display: none; }`,
+    releases: getCSSStyle('release'),
+    stars: getCSSStyle('watch_started'),
+    follows: getCSSStyle('follow'),
+    forks: getCSSStyle('fork'),
+    push: getCSSStyle('push'),
+    sponsors: getCSSStyle('sponsor')
 };
 
 export const STORAGE_KEY = 'refinedGithubData';
